10470a9b7a9d32dd940e7fbbf0fd7d243f257f68
Daniel MUGISHA <dmugisha189@gmail.com>
2021-11-12 13:21:22 +0200

    final commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-window.onload = () => {
-	document.querySelector(".container").innerHTML = "<h1>Let's do it!</h1>";
+let actualValue = 0;
+const element = document.querySelector("#currentValue");
+
+const calculateMax = () => {
+	const number1 = parseInt(document.getElementById("number1").value);
+	const number2 = parseInt(document.getElementById("number2").value);
+	return number1 + number2;
+};
+
+const increment = () => {
+	const maxLimit = calculateMax();
+	if (actualValue < maxLimit) {
+		actualValue = actualValue + 1;
+	}
+	element.innerHTML = `<h1>${actualValue}</h1>`;
+};
+
+const decrement = () => {
+	if (actualValue > 0) {
+		actualValue = actualValue - 1;
+	}
+	element.innerHTML = `<h1>${actualValue}</h1>`;
 };
